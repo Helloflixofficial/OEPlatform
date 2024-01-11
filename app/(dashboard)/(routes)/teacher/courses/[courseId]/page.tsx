b9e384903c6352ce723ccf68e8b4987acb89ff22
Ɓʘɓƴ <shboby71@gmail.com>
2024-01-11 23:32:40 +0530

    16 easy

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,33 @@
-const CourseIdPage = () => {
-    return ( <div>
-        course Id page is here
-    </div> );
-}
- 
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+    // userId verifying
+  const userId = auth();
+  if (!userId) {
+    return;
+    redirect("/");
+  }
+
+  //course id verifying
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  });
+  if (!course) {
+    return redirect("/");
+  }
+
+const requireFields = [
+    course.title,
+    course.imageUrl,
+    course.description,
+    course.price,
+    course.categoryId
+]
+
+
+  return <div>course Id is = {params.courseId}</div>;
+};
 export default CourseIdPage;

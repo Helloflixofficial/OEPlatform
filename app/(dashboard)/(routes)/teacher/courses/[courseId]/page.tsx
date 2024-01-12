339ea5358f78dfe7fc6096bba91634e98a3d0835
Ɓʘɓƴ <shboby71@gmail.com>
2024-01-12 23:07:59 +0530

    17 Nothing special some random functions etc

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,8 +1,9 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
-    // userId verifying
+  // userId verifying
   const userId = auth();
   if (!userId) {
     return;
@@ -19,15 +20,35 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     return redirect("/");
   }
 
-const requireFields = [
+  const requireFields = [
     course.title,
     course.imageUrl,
     course.description,
     course.price,
-    course.categoryId
-]
+    course.categoryId,
+  ];
 
+  const totalField = requireFields.length;
+  const completedField = requireFields.filter(Boolean).length;
+  const completedData = `(${completedField}/${totalField})`;
 
-  return <div>course Id is = {params.courseId}</div>;
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-center">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Coures Here</h1>
+          <span className="text-slate-700 text-sm ">
+            Completed Task{completedData}
+          </span>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1  md:grid-cols-2 gap-6 mt-16">
+        <div className="flex items-center gap-x-2">
+          <h1 className="text-xl">CUSTOMIZE YOUR CODE HERE</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default CourseIdPage;

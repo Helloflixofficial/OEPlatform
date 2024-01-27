774cddb5bbdfcaef646ed5093a17ba0bf991b8bf
Ɓʘɓƴ <shboby71@gmail.com>
2024-01-27 22:32:42 +0530

    Happy Bug Day 28

--- components/file-upload.ts
+++ components/file-upload.ts
@@ -1,14 +1,19 @@
-"use Client";
+"use client";
+
+import toast from "react-hot-toast";
+
 import { UploadDropzone } from "@/lib/uploadthing";
 import { ourFileRouter } from "@/app/api/uploadthing/core";
-import toast from "react-hot-toast";
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
   endpoint: keyof typeof ourFileRouter;
-}
+};
 
-export const FileUpload = ({ onChange, endpoint }: FileUploadProps) => {
+export const FileUpload = ({
+  onChange,
+  endpoint
+}: FileUploadProps) => {
   return (
     <UploadDropzone
       endpoint={endpoint}
@@ -16,10 +21,8 @@ export const FileUpload = ({ onChange, endpoint }: FileUploadProps) => {
         onChange(res?.[0].url);
       }}
       onUploadError={(error: Error) => {
-        toast.error(
-          `${error?.message}`
-        );
+        toast.error(`${error?.message}`);
       }}
     />
-  );
-};
+  )
+}

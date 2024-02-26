b633562486190b62889512ae5530e4697036b8f8
Ɓʘɓƴ <shboby71@gmail.com>
2024-02-26 23:02:24 +0530

    Chapter Ui drag and Drop 52

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_component/chapters-list.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_component/chapters-list.tsx
@@ -8,6 +8,7 @@ import {
   DropResult,
 } from "@hello-pangea/dnd";
 import { cn } from "@/lib/utils";
+import { Badge, Grip, Pencil } from "lucide-react";
 
 interface ChapterListProps {
   items: Chapter[];
@@ -53,8 +54,40 @@ export const ChaptersList = ({
                 >
                   {(provided) => (
                     <div
-                      className={cn("flex items-center gap-x-2 bg-slate-200")}
-                    ></div>
+                      className={cn(
+                        "flex items-center gap-x-2 bg-slate-200 border-slate-200 border text-slate-700 rounded-md mb-4 text-sm",
+                        chapter.isPublished && "bg-sky-100 border-sky-200"
+                      )}
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                    >
+                      <div
+                        className={cn(
+                          "px-2 py-3 border-r border-r-slate-200 hover:bg-slate-300 rounded-l-md transition",
+                          chapter.isPublished &&
+                            "border-r-sky-200  hover:bg-sky-200"
+                        )}
+                        {...provided.dragHandleProps}
+                      >
+                        <Grip className="h-5 w-5" />
+                      </div>
+                      {chapter.title}
+                      <div className="ml-auto pr-2 flex items-center gap-x-2">
+                        {chapter.isFree && <Badge>Free</Badge>}
+                        <Badge
+                          className={cn(
+                            "bg-slate-500",
+                            chapter.isPublished && "bg-sky-700"
+                          )}
+                        >
+                          {chapter.isPublished ? "Published" : "Draft"}
+                        </Badge>
+                        <Pencil
+                          onClick={() => onEdit(chapter.id)}
+                          className="w-4 h-4 cursor-pointer hover:opacity-75 transition"
+                        />
+                      </div>
+                    </div>
                   )}
                 </Draggable>
               ))}

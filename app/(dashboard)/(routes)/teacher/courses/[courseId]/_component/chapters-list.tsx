a424efe3f29ebc5e898255db3911c2216d0b2ed4
Ɓʘɓƴ <shboby71@gmail.com>
2024-02-29 23:02:40 +0530

    still error 56

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_component/chapters-list.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_component/chapters-list.tsx
@@ -37,16 +37,15 @@ export const ChaptersList = ({
     setIsMounted(true);
   }, []);
 
-
   const onDragEnd = (result: DropResult) => {
     if (!result.destination) return;
     const items = Array.from(chapter);
     const [reorderedItem] = items.splice(result.source.index, 1);
     items.splice(result.destination.index, 0, reorderedItem);
-  
+
     const startIndex = Math.min(result.source.index, result.destination.index);
     const endIndex = Math.max(result.source.index, result.destination.index);
-  
+
     const updateChapter = items.slice(startIndex, endIndex + 1);
     setChapter(items);
     const bulkUpdatedData = updateChapter.map((chapter) => ({
@@ -55,7 +54,6 @@ export const ChaptersList = ({
     }));
     onReorder(bulkUpdatedData);
   };
-  
 
   if (!isMounted) {
     return null;
--- app/api/courses/[courseId]/chapters/onRender/route.ts
+++ app/api/courses/[courseId]/chapters/onRender/route.ts
@@ -1,10 +1,18 @@
+import { db } from '@/lib/db';
 import { NextResponse } from "next/server";
 
 export async function PUT(
   req: Request,
-  { params }: { params: { courseId: string } }
+  { params, userId }: { params: { courseId: string }, userId: string }
 ) {
   try {
+    const { list } = await req.json();
+    const ownCoures = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
+      }
+    });
   } catch (error) {
     console.log("[REORDER]", error);
     return new NextResponse("Internal Error", { status: 500 });

49a05e9fcfa0a9ffaba91522807050c04a248221
Ɓʘɓƴ <shboby71@gmail.com>
2024-03-28 23:00:23 +0530

    75 key get created

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -15,6 +15,86 @@ if (
   throw new Error("Missing or invalid Mux token ID or secret");
 }
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("UNAUT USER ", { status: 401 });
+    }
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!ownCourse) {
+      return new NextResponse("UNAUT", { status: 401 });
+    }
+
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+    });
+
+    if (!chapter) {
+      return new NextResponse("CHAPTER NOT FOUND", { status: 404 });
+    }
+    if (chapter.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
+
+      if (existingMuxData) {
+        await Video.Assets.del(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+    }
+
+    const deleteChapter = await db.chapter.delete({
+      where: {
+        id: params.chapterId,
+      },
+    });
+
+    const PublishedChaptersInCourse = await db.chapter.findMany({
+      where: {
+        courseId: params.courseId,
+        isPublished: true,
+      },
+    });
+
+    if (PublishedChaptersInCourse.length) {
+      await db.course.update({
+        where: {
+          id: params.courseId,
+        },
+        data: {
+          isPublished: false,
+        },
+      });
+    }
+
+    return NextResponse.json(deleteChapter);
+  } catch (error) {
+    console.log("[CAPTER_ID_DELETE]", error);
+    return new NextResponse("INTERNAL ERROR", { status: 500 });
+  }
+}
+
+// This part of function
 export async function PATCH(
   req: Request,
   { params }: { params: { courseId: string; chapterId: string } }
@@ -22,7 +102,7 @@ export async function PATCH(
   try {
     const { userId } = auth();
     const { isPublished, ...values } = await req.json();
-    
+
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
@@ -78,7 +158,6 @@ export async function PATCH(
         },
       });
     }
-
 
     return NextResponse.json(chapter);
   } catch (error) {

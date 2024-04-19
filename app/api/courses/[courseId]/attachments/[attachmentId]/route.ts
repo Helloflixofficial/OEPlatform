5becaed89f575a77124f0a4d9797fc3a42359621
Ɓʘɓƴ <shboby71@gmail.com>
2024-04-19 21:47:13 +0530

    84 random errors

--- app/api/courses/[courseId]/attachments/[attachmentId]/route.ts
+++ app/api/courses/[courseId]/attachments/[attachmentId]/route.ts
@@ -1,40 +1,41 @@
-import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { auth } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
 
-import { db } from "@/lib/db";
+import { db } from '@/lib/db'
+import { isTeacher } from '@/lib/teacher'
 
 export async function DELETE(
   req: Request,
-  { params }: { params: { courseId: string; attachmentId: string } }
+  { params }: { params: { courseId: string; attachmentId: string } },
 ) {
   try {
-    const { userId } = auth();
+    const { userId } = auth()
+    const { courseId, attachmentId } = params
 
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
+    if (!userId || !isTeacher(userId)) {
+      return new NextResponse('Unauthorized', { status: 401 })
     }
 
     const courseOwner = await db.course.findUnique({
       where: {
-        id: params.courseId,
-        userId: userId,
+        userId,
+        id: courseId,
       },
-    });
+    })
 
     if (!courseOwner) {
-      return new NextResponse("Unauthorized", { status: 401 });
+      return new NextResponse('Unauthorized', { status: 401 })
     }
 
     const attachment = await db.attachment.delete({
       where: {
-        courseId: params.courseId,
-        id: params.attachmentId,
+        id: attachmentId,
       },
-    });
+    })
 
-    return NextResponse.json(attachment);
+    return NextResponse.json(attachment)
   } catch (error) {
-    console.log("ATTACHMENT_ID", error);
-    return new NextResponse("Internal Error", { status: 500 });
+    console.error('[ATTACHMENT_ID]', error)
+    return new NextResponse('Internal server error', { status: 500 })
   }
 }
--- app/api/courses/[courseId]/attachments/route.ts
+++ app/api/courses/[courseId]/attachments/route.ts
@@ -1,46 +1,48 @@
-import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
-import { db } from "@/lib/db";
+import { auth } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
+
+import { db } from '@/lib/db'
+import { isTeacher } from '@/lib/teacher'
 
 export async function POST(
   req: Request,
-  { params }: { params: { courseId: string } }
+  { params }: { params: { courseId: string } },
 ) {
   try {
-    const { userId } = auth();
-    const { url } = await req.json();
+    const { userId } = auth()
+    const { courseId } = params
+    const { url } = await req.json()
 
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
+    if (!userId || !isTeacher(userId)) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    if (!url) {
+      return new NextResponse('File URL is required', { status: 400 })
     }
 
     const courseOwner = await db.course.findUnique({
       where: {
-        id: params.courseId,
-        userId: userId,
-      }
-    });
+        userId,
+        id: courseId,
+      },
+    })
 
     if (!courseOwner) {
-      return new NextResponse("Unauthorized", { status: 401 });
+      return new NextResponse('Unauthorized', { status: 401 })
     }
-
 
     const attachment = await db.attachment.create({
       data: {
         url,
-        name: url.split("/").pop(),
-        courseId: params && params.courseId, // Add a check for params and courseId
+        courseId,
+        name: url.split('/').pop(),
       },
-    });
-    
+    })
 
-
-
-
-    return NextResponse.json(attachment);
+    return NextResponse.json(attachment)
   } catch (error) {
-    console.log("COURSE_ID_ATTACHMENTS", error);
-    return new NextResponse("Internal Error", { status: 500 });
+    console.error('[COURSE_ID_ATTACHMENTS]', error)
+    return new NextResponse('Internal server error', { status: 500 })
   }
 }

167fd0477ccaa89bf08f532b25d9a5672acdceb5
Ɓʘɓƴ <shboby71@gmail.com>
2024-06-06 22:44:09 +0530

    98 need help here

---
+++ components/models/conferm-model.tsx
@@ -0,0 +1,35 @@
+"use client";
+import {
+  AlertDialog,
+  AlertDialogTitle,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogTrigger,
+  AlertDialogDescription,
+} from "@/components/ui/alert-dialog";
+interface ConfirmModalProps {
+  onConfirm: () => void;
+  children: React.ReactNode;
+}
+export const ConfirmModel = ({ children, onConfirm }: ConfirmModalProps) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>How are you</AlertDialogTitle>
+          <AlertDialogDescription>
+            This ation cannot be Undone he he
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};

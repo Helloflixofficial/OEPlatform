2214e0712feac64564f2ca5ceb59a4fd2662af39
Ɓʘɓƴ <shboby71@gmail.com>
2024-06-20 22:06:12 +0530

    105 databox

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,11 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { columns } from "./[courseId]/_components/columns";
+import { DataTable } from "./[courseId]/_components/data-table";
 import Link from "next/link";
-const TeacherPage = () => {
+async function getData(): Promise<any[]> {
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "hellolixofficial@gmail.com",
+    },
+  ];
+}
+
+const TeacherPage = async () => {
+  const data = await getData();
+
   return (
     <div className="p-6">
       <Link href="/teacher/create">
         <Button>Button</Button>
       </Link>
+      <DataTable columns={columns} data={data} />
     </div>
   );
 };

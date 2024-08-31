6d8cb82e63f3dd3b128a1cffaddfd8995ce3475e
Ɓʘɓƴ <shboby71@gmail.com>
2024-08-31 22:53:11 +0530

    the table is labeled

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/columns.tsx
@@ -1,28 +1,40 @@
 "use client";
-import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react";
+
+import Link from "next/link";
 import { Course } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { Button } from "@/components/ui/button";
+import { ArrowUpDown, MoreHorizontal, Pencil, X } from "lucide-react";
 
+import { cn } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
-  DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
+  DropdownMenuContent,
 } from "@/components/ui/dropdown-menu";
-import Link from "next/link";
+
 export const columns: ColumnDef<Course>[] = [
   {
-    accessorKey: "tittle",
+    accessorKey: "title",
     header: ({ column }) => {
       return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Title
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
+        <div className="flex flex-row">
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Title
+            <ArrowUpDown className="w-4 h-4 ml-2" />
+          </Button>
+
+          {column.getIsSorted() && (
+            <Button variant="ghost" onClick={() => column.clearSorting()}>
+              <X className="w-4 h-4" />
+            </Button>
+          )}
+        </div>
       );
     },
   },
@@ -30,27 +42,61 @@ export const columns: ColumnDef<Course>[] = [
     accessorKey: "price",
     header: ({ column }) => {
       return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Price
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
+        <div className="flex flex-row">
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Price
+            <ArrowUpDown className="w-4 h-4 ml-2" />
+          </Button>
+
+          {column.getIsSorted() && (
+            <Button variant="ghost" onClick={() => column.clearSorting()}>
+              <X className="w-4 h-4" />
+            </Button>
+          )}
+        </div>
       );
+    },
+    cell: ({ row }) => {
+      const price = parseFloat(row.getValue("price") || "0");
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(price);
+
+      return <div>{formatted}</div>;
     },
   },
   {
     accessorKey: "isPublished",
     header: ({ column }) => {
       return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Published
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
+        <div className="flex flex-row">
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Published
+            <ArrowUpDown className="w-4 h-4 ml-2" />
+          </Button>
+
+          {column.getIsSorted() && (
+            <Button variant="ghost" onClick={() => column.clearSorting()}>
+              <X className="w-4 h-4" />
+            </Button>
+          )}
+        </div>
+      );
+    },
+    cell: ({ row }) => {
+      const isPublished = row.getValue("isPublished") || false;
+
+      return (
+        <Badge className={cn("bg-slate-500", isPublished && "bg-sky-700")}>
+          {isPublished ? "Published" : "Draft"}
+        </Badge>
       );
     },
   },
@@ -58,44 +104,20 @@ export const columns: ColumnDef<Course>[] = [
     id: "actions",
     cell: ({ row }) => {
       const { id } = row.original;
+
       return (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-4 w-8 p-0">
-              <span className="sr-only">
-                Open Menu
-                <MoreHorizontal />
-              </span>
+            <Button variant="ghost" className="w-8 h-4 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="w-4 h-4" />
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <Link href={`/teacher/courses${id}`}>
-              <DropdownMenuItem>
-                <Pencil className="h-4 m-4 mr-2" />
-                Edit
-              </DropdownMenuItem>
-            </Link>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
-  {
-    id: "actions",
-    cell: ({ row }) => {
-      const { id } = row.original;
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-4 w-8 p-0">
-              <span className="sr-only">Open Menu</span>
-              <MoreHorizontal />
-            </Button>
-          </DropdownMenuTrigger>
+
           <DropdownMenuContent align="end">
             <Link href={`/teacher/courses/${id}`}>
-              <DropdownMenuItem>
-                <Pencil className="h-4 w-4 mr-2" />
+              <DropdownMenuItem className="cursor-pointer">
+                <Pencil className="w-4 h-4 mr-2" />
                 Edit
               </DropdownMenuItem>
             </Link>

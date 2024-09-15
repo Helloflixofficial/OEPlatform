336e60465c4e95db9d71db617dc350642da7eec1
Ɓʘɓƴ <shboby71@gmail.com>
2024-09-15 21:44:30 +0530

    searach bar routes 116

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,6 +1,7 @@
 // "use client";
 import { db } from "@/lib/db";
 import  { Categories } from "./_components/Categories";
+import { Searchinput } from "@/components/navbarroutes/search-input";
 const searchPage =  async () => {
   const categories = await db.category.findMany({
     orderBy: {
@@ -8,10 +9,14 @@ const searchPage =  async () => {
     },
   });
   return (
-    <div className="p-6">
+    <>
+    <div className="px-6 pt-6 md:hidden md:mb-0 block">
+      <Searchinput />
+    </div> 
+    <div className="p-6 ">
       <Categories items={categories} /><table></table>
     </div>
+    </>
   );
 };
-
 export default searchPage;
--- components/navbarroutes/search-input.tsx
+++ components/navbarroutes/search-input.tsx
@@ -1,6 +1,35 @@
 "use client"
+import { useState,useEffect } from "react"
+import { SearchIcon } from "lucide-react"
+import { Input } from "@/components/ui/input"
+import { useDebounce } from "@/hooks/use.debounce"
+import { useSearchParams,useRouter , usePathname } from "next/navigation"
+import qs from "query-string" 
 export const Searchinput = () => {
+const [value, setValue] = useState("")
+const debounceVlaue = useDebounce(value)
+const searchParamas = useSearchParams(); 
+const router = useRouter();
+const pathname = usePathname();
+const currentCategoryId = searchParamas.get("categoryId")
+
+useEffect(() => {
+  const url = qs.stringifyUrl({
+      url: pathname,
+      query: {
+        CategoriesId : currentCategoryId,
+        title : debounceVlaue 
+      }
+  },{skipEmptyString : true, skipNull : true})
+  router.push(url)
+},[debounceVlaue, currentCategoryId, router , pathname])
   return (
-    <div>search-input</div>
+    <div className="relative ">
+      <SearchIcon className="h-4 w-4 absolute top-3 left-3 text-slate-600" />
+      <Input onChange={(e) => 
+           setValue(e.target.value)
+       } value={value} className="w-full  md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible: ring-slate-200" 
+       placeholder="Seach courses"/>
+    </div>
   )
 }

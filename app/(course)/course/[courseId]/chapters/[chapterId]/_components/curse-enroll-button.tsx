c7ff9855b5ba864c8bef9690ba2ef54f7466a5be
Ɓʘɓƴ <shboby71@gmail.com>
2024-10-13 22:18:36 +0530

    now its updates and some rendom error 128

--- app/(course)/course/[courseId]/chapters/[chapterId]/_components/curse-enroll-button.tsx
+++ app/(course)/course/[courseId]/chapters/[chapterId]/_components/curse-enroll-button.tsx
@@ -38,6 +38,7 @@ export const CurseEnrollButton = ({
     })
   }
 
+
   return (
     <Button
       size="sm"
--- app/api/courses/[courseId]/checkout/route.ts
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -80,8 +80,8 @@ export async function POST(
       customer: stripeCustomer.stripeCustomerId,
       line_items,
       mode: 'payment',
-      success_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}?success=1`,
-      cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}?canceled=1`,
+      success_url: `${process.env.NEXT_PUBLIC_APP_URL}/course/${course.id}?success=1`,
+      cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/course/${course.id}?canceled=1`,
       metadata: {
         courseId: course.id,
         userId: user.id,

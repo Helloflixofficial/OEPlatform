794b3374b15b43e905c27a78a8fc1145e3a04b4c
Ɓʘɓƴ <shboby71@gmail.com>
2024-12-17 22:25:56 +0530

    137 testin

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,22 +1,41 @@
-import { isTeacher } from "@/lib/teacher";
-import { auth } from "@clerk/nextjs";
-import { createUploadthing, type FileRouter } from "uploadthing/next";
-const f = createUploadthing();
+import { auth } from '@clerk/nextjs'
+import { utapi } from 'uploadthing/server'
+import { createUploadthing, type FileRouter } from 'uploadthing/next'
+
+import { isTeacher } from '@/lib/teacher'
+
+const f = createUploadthing()
+
 const handleAuth = () => {
-  const { userId } = auth();
+  const { userId } = auth()
   const isAuthorized = isTeacher(userId)
-  if (!userId || !isAuthorized) throw new Error("Unauthorized");
-  return { userId };
-};
+
+  if (!userId || !isAuthorized) throw new Error('Unauthorized')
+  return { userId }
+}
+
 export const ourFileRouter = {
-  courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+  courseImage: f({ image: { maxFileSize: '16MB', maxFileCount: 1 } })
     .middleware(() => handleAuth())
+    .onUploadError(async error => {
+      console.error('[UPLOADTHING]', error)
+      await utapi.deleteFiles(error.fileKey)
+    })
     .onUploadComplete(() => { }),
-  courseAttachment: f(["text", "image", "video", "audio", "pdf"])
+  courseAttachment: f(['text', 'image', 'video', 'audio', 'pdf'])
     .middleware(() => handleAuth())
+    .onUploadError(async error => {
+      console.error('[UPLOADTHING]', error)
+      await utapi.deleteFiles(error.fileKey)
+    })
     .onUploadComplete(() => { }),
-  chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: "512GB" } })
+  chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: '512GB' } })
     .middleware(() => handleAuth())
+    .onUploadError(async error => {
+      console.error('[UPLOADTHING]', error)
+      await utapi.deleteFiles(error.fileKey)
+    })
     .onUploadComplete(() => { }),
-} satisfies FileRouter;
-export type OurFileRouter = typeof ourFileRouter;
+} satisfies FileRouter
+
+export type OurFileRouter = typeof ourFileRouter
--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,8 +1,7 @@
-import { createNextRouteHandler } from "uploadthing/next";
- 
-import { ourFileRouter } from "./core";
- 
-// Export routes for Next App Router
+import { createNextRouteHandler } from 'uploadthing/next'
+
+import { ourFileRouter } from './core'
+
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
-});
+})
